2cefe92cd9dccdaae999c809c463f3b916fd1e55
cephaske254 <cephaske254@gmail.com>
2021-03-03 20:33:20 +0300

    initial commit

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // truncate existing records to start from scratch
+
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            Article::create([
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph,
+            ]);
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('articles', function () {
+    return Article::all();
+});
+Route::get('articles/{id}', function ($id) {
+    return Article::findOrFail($id);
+});
+
+Route::post('articles', function (Request $request) {
+    return Article::create($request->all());
+});
+
+Route::put('articles/{id}', function (Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all);
+    return $article;
+});
